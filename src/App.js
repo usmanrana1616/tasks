1b467edd2486c13c77584d79d74e4a7b37cfba39
usmanrana1616 <usmanrana1616@gmail.com>
2021-09-30 00:00:03 +0500

    currency converter

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react';
 function App() {
   const [count, setCount] = useState(0);
   const [background, setbackground] = useState("gray");
-
+ 
   var value = 25;
   useEffect(() => {
     var inter = setInterval(() => {
@@ -25,10 +25,39 @@ function App() {
       }
     }, 1000);
   }, []);
+
+  const [pkr, setPKR] = useState(170);
+  const [dollar, setDollar] = useState(1);
+
+  useEffect(() => {
+    setDollar(pkr/170)
+  }, [pkr])
+  useEffect(() => {
+    setPKR(dollar*170)
+  }, [dollar])
   return (
-    <div style={{display:'flex',flexDirection:'row',textAlign:'center',marginTop:20,  justifyContent:'center', }}>
+    <div style={{textAlign:'center',marginTop:20,  justifyContent:'center', }}>
       <div style={{ background, color: "white", height: 30, width: 100 }}>
         Count: {count}
+      </div>
+      <div >
+
+      <div>
+        <text>Enter Dollar</text>
+        <input
+          value={dollar}
+          placeholder={"Enter dollar"}
+          onChange={(e) => setDollar(e.target.value)}
+        />
+      </div>
+      <div>
+      <text>Enter Pkr</text>
+        <input
+          value={pkr}
+          placeholder={"Enter pkr"}
+          onChange={(e) => setPKR(e.target.value)}
+        />
+      </div>
       </div>
     </div>
 
